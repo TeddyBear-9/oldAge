9d31626937f827cbde6492a6a30293603823bad0
teddy bear <857719324@qq.com>
2022-07-01 22:37:55 +0800

    连接数据库，创建第一个app:home

--- oldAge/settings.py
+++ oldAge/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'oldAge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oldAge',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '39.105.102.68',
+        'POST': '3306',
     }
 }
 
