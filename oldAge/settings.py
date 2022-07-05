d1e33c2fbe51072c0639850fe8add724eafaa8f0
teddy bear <857719324@qq.com>
2022-07-05 17:38:35 +0800

    fix(setting.py):修改跨域访问中间件位置

--- oldAge/settings.py
+++ oldAge/settings.py
@@ -50,12 +50,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # 跨域中间件
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware', # 跨域中间件
+
 ]
 
 ROOT_URLCONF = 'oldAge.urls'
