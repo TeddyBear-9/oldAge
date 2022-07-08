df36d15994f5ab0c2977977090e6a3167444295c
teddy bear <857719324@qq.com>
2022-07-08 14:11:09 +0800

    fix(urls.py):增加图片获取路径

--- oldAge/urls.py
+++ oldAge/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg2.views import get_schema_view
 from drf_yasg2 import openapi
+from django.conf import settings
+from django.views.static import serve
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -43,6 +45,7 @@ urlpatterns = [
     re_path(r'^doc(?P<format>\.json|\.yaml)$',schema_view.without_ui(cache_timeout=0), name='schema-json'),  #<-- 这里
     path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  #<-- 这里
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  #<-- 这里
+    re_path(r'media/(?P<path>.*)', serve, {"document_root": settings.MEDIA_ROOT}),
 
 ]
 
