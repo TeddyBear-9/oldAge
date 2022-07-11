686e762d095748c3c2b242a322fa6e71ffe39edb
teddy bear <857719324@qq.com>
2022-07-11 11:29:42 +0800

    feat(views.py):增加数据统计接口

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from rest_framework.decorators import action
 from rest_framework import viewsets, status
@@ -8,7 +10,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt import authentication
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView, RetrieveAPIView, \
     GenericAPIView, DestroyAPIView, UpdateAPIView
-
 from .serializers import *
 
 
@@ -26,6 +27,15 @@ class OldPersonModelView(ModelViewSet):
         instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    @action(methods=['GET'], detail=False, url_path="statistics")
+    def get_statistics(self, request, *args, **kwargs):
+        now = datetime.date.today().strftime("%Y-%m-%d")
+        old_o80 = OldPerson.get_all().filter(birthday__lt='1942-01-01').count()
+        old_y80_o70 = OldPerson.get_all().filter(birthday__gt='1942-01-01', birthday__lt='1952-01-01').count()
+        old_y70_o60 = OldPerson.get_all().filter(birthday__gt='1952-01-01', birthday__lt='1962-01-01').count()
+        old_y60 = OldPerson.get_all().filter(birthday__gt='1962-01-01').count()
+        return Response(data=[old_o80, old_y80_o70, old_y70_o60, old_y60], status=status.HTTP_200_OK)
+
 
 class EmployeeModelView(ModelViewSet):
     queryset = Employee.get_all()
@@ -37,6 +47,15 @@ class EmployeeModelView(ModelViewSet):
         instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    @action(methods=['GET'], detail=False, url_path="statistics")
+    def get_statistics(self, request, *args, **kwargs):
+        now = datetime.date.today().strftime("%Y-%m-%d")
+        emy_o80 = Employee.get_all().filter(birthday__lt='1942-01-01').count()
+        emy_y80_o70 = Employee.get_all().filter(birthday__gt='1942-01-01', birthday__lt='1952-01-01').count()
+        emy_y70_o60 = Employee.get_all().filter(birthday__gt='1952-01-01', birthday__lt='1962-01-01').count()
+        emy_y60 = Employee.get_all().filter(birthday__gt='1962-01-01').count()
+        return Response(data=[emy_o80, emy_y80_o70, emy_y70_o60, emy_y60], status=status.HTTP_200_OK)
+
 
 class VolunteerModelView(ModelViewSet):
     queryset = Volunteer.get_all()
@@ -47,6 +66,15 @@ class VolunteerModelView(ModelViewSet):
         instance.remove = '已删除'
         instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['GET'], detail=False, url_path="statistics")
+    def get_statistics(self, request, *args, **kwargs):
+        now = datetime.date.today().strftime("%Y-%m-%d")
+        vlt_o80 = Volunteer.get_all().filter(birthday__lt='1942-01-01').count()
+        vlt_y80_o70 = Volunteer.get_all().filter(birthday__gt='1942-01-01', birthday__lt='1952-01-01').count()
+        vlt_y70_o60 = Volunteer.get_all().filter(birthday__gt='1952-01-01', birthday__lt='1962-01-01').count()
+        vlt_y60 = Volunteer.get_all().filter(birthday__gt='1962-01-01').count()
+        return Response(data=[vlt_o80, vlt_y80_o70, vlt_y70_o60, vlt_y60], status=status.HTTP_200_OK)
 
 
 class RegisterView(CreateAPIView):
